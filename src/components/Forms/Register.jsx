0da757011528c6e725ca38bf084f8624f723d996
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-27 23:45:45 +0530

    registration implemented with error display

--- src/components/Forms/Register.jsx
+++ src/components/Forms/Register.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import fire from "../../config/Fire";
 
 export default class Register extends Component {
   state = {
     email: "",
     password: "",
     displayName: "",
-    fileErrors: "",
+    fireErrors: "",
   };
 
   handleChange = (e) => {
@@ -13,9 +14,35 @@ export default class Register extends Component {
       [e.target.name]: e.target.value,
     });
   };
+
+  register = (e) => {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((user) => {
+        //          var currentUser = fire.auth().currentUser:
+        var currentUser = fire.auth().currentUser;
+        currentUser.updateProfile({
+          displayName: this.state.displayName,
+        });
+      })
+      .catch((error) => {
+        this.setState({ fireErrors: error.message });
+      });
+  };
+
   render() {
+    let errorNotification = this.state.fireErrors ? (
+      <div className="error">{this.state.fireErrors}</div>
+    ) : null;
+    // let errorNotification = this.state.fireErrors ? (
+    //   <div className="Error"> {this.state.fireErrors} </div>
+    // ) : null;
+
     return (
-      <>
+      <div>
+        {errorNotification}
         <form>
           <input
             type="text"
@@ -41,9 +68,14 @@ export default class Register extends Component {
             value={this.state.password}
             onChange={this.handleChange}
           />
-          <input type="submit" className="submitBtn" value="REGISTER" />
+          <input
+            type="submit"
+            className="submitBtn"
+            value="REGISTER"
+            onClick={this.register}
+          />
         </form>
-      </>
+      </div>
     );
   }
 }
