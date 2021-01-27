cf071246f3ef2b5ba5c55d854a0766585bff1324
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-27 20:15:52 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Salary and Expense Tracker</h1>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
