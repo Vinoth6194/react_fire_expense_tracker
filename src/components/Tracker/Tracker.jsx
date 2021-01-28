6c0c9aa0ad43d5ebde5290b47df3ca40d97939e3
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-28 14:18:40 +0530

    transactions form and list

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -7,10 +7,46 @@ export default class Tracker extends Component {
   };
   render() {
     return (
-      <div>
-        Youare logged in
-        <button onClick={this.logout}>Logout</button>
-      </div>
+      <>
+        <div className="trackerBlock">
+          <div className="welcome">
+            <span>Hi, Username!</span>
+            <button className="exit" onClick={this.logout}>
+              Exit
+            </button>
+          </div>
+          <div className="totalMoney">$145</div>
+          <div className="newTransactionBlock">
+            <div className="newTransaction">
+              <form>
+                <input
+                  type="text"
+                  placeholder="Transaction Name"
+                  name="transactionName"
+                />
+                <div className="inputGroup">
+                  <select name="type">
+                    <option value="0">Type</option>
+                    <option value="expense">Expense</option>
+                    <option value="deposit">Deposit</option>
+                  </select>
+                  <input type="text" placeholder="Price" name="price" />
+                </div>
+                <button className="addTransaction">+ Add Transaction</button>
+              </form>
+            </div>
+          </div>
+          <div className="latestTransactions">
+            <p>Latest Transactions</p>
+            <ul>
+              <li>
+                <div>ATM Deposit</div>
+                <div>+$5</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
     );
   }
 }
